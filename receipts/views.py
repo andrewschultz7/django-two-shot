dffa8c8761efb528608f0f60606f847cae8cf3e1
Andew Schultz <andrew7primary@gmail.com>
2022-10-19 15:25:03 -0600

    Feature 5 before test for some reason

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import ExpenseCategory, Receipt, Account
 
 # Create your views here.
+
+
+def show_receipt(request):
+    receipt = Receipt.objects.all()
+    context = {
+        "receipts": receipt,
+    }
+    return render(request, "receipts/receipts.html", context)
